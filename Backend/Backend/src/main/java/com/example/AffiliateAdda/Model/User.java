35cd31a591911e92c28530e07540c49f7ce945e1
harshil3030 <savaliyaharshil3030@gmail.com>
2025-09-06 23:54:05 +0530

    updating properties/pom.xml and usermodel

--- Backend/Backend/src/main/java/com/example/AffiliateAdda/Model/User.java
+++ Backend/Backend/src/main/java/com/example/AffiliateAdda/Model/User.java
@@ -1,10 +1,28 @@
 package com.example.AffiliateAdda.Model;
 
+import jakarta.persistence.*;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Component
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String role = "USER";
+
+    @Column(nullable = false)
     private boolean active = true;
-//    private DateTime creationDate;
+
+    private DateTime creationDate;
 }
