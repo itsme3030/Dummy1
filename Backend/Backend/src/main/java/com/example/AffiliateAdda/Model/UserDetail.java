d76a70905a440b2cb6e081367149ef3241c8b7f2
harshil3030 <savaliyaharshil3030@gmail.com>
2025-09-07 00:09:00 +0530

    user and userdetails updated

--- Backend/Backend/src/main/java/com/example/AffiliateAdda/Model/UserDetail.java
+++ Backend/Backend/src/main/java/com/example/AffiliateAdda/Model/UserDetail.java
@@ -1,8 +1,22 @@
 package com.example.AffiliateAdda.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="user_detail")
 public class UserDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userDetailId;
 
+    @OneToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
     private double totalWithdrawal = 0L;
@@ -16,4 +30,100 @@ public class UserDetail {
     private String state;
     private String zip;
     private String country;
+
+    public Long getUserDetailId() {
+        return userDetailId;
+    }
+
+    public void setUserDetailId(Long userDetailId) {
+        this.userDetailId = userDetailId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public double getTotalWithdrawal() {
+        return totalWithdrawal;
+    }
+
+    public void setTotalWithdrawal(double totalWithdrawal) {
+        this.totalWithdrawal = totalWithdrawal;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
